89c353fcf471c4c84cb37dbfcf82a5509644bee6
Bharatvaj <bharatvajph@gmail.com>
2017-11-26 09:35:57 +0530

    node::Node::readln() now returns const char *

--- include/Node/Node.hpp
+++ include/Node/Node.hpp
@@ -1,19 +1,19 @@
 #ifndef _NODE
 #define _NODE "NODE"
-#include <iostream>
 #include <cstring>
+#include <iostream>
 #if defined(__linux__) || defined(__APPLE__)
 
 #if defined(__linux__) && defined(kernel_version_2_4)
 #include <sys/sendfile.h>
 #endif
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 typedef int SOCKET;
 #define INVALID_SOCKET -1
 
@@ -43,54 +43,51 @@ typedef int ssize_t;
 #define log_fat(...)
 #endif
 
-namespace node
-{
-  class Node
-  {
-  protected:
-    SOCKET node_sock;
-    SOCKET start_server (int);
-    SOCKET connect_server (const char *, int);
-  public:
-#define SH_BUFSIZE 1024		//shell read buffer size
+namespace node {
+class Node {
+protected:
+  SOCKET node_sock;
+  SOCKET start_server(int);
+  SOCKET connect_server(const char *, int);
+
+public:
+#define SH_BUFSIZE 1024 // shell read buffer size
 #define SH_TOK_BUFSIZE 64
-#define SH_TOK_DELIM " \t\r\n\a"	//shell token delimeter
+#define SH_TOK_DELIM " \t\r\n\a" // shell token delimeter
 
-    typedef struct
-    {
-      const char *word;
-      const char letter;
-      const char *description;
-    } option;
+  typedef struct {
+    const char *word;
+    const char letter;
+    const char *description;
+  } option;
 
-    typedef struct
-    {
-      const char *command;
-      const char *info;
-      int (*function) (int, char **, Node *);
-      int opt_length;
-      option *options;
-    } job;
+  typedef struct {
+    const char *command;
+    const char *info;
+    int (*function)(int, char **, Node *);
+    int opt_length;
+    option *options;
+  } job;
 
-      Node (void);
-      Node (SOCKET);
-     ~Node (void);
+  Node(void);
+  Node(SOCKET);
+  ~Node(void);
 
-    int writeln (const char *);
-    int writeln (const char *, int);
-    char *readln (void);
+  int writeln(const char *);
+  int writeln(const char *, int);
+  const char *readln(void);
 #if defined(__linux__) || defined(__APPLE__)
-    int write_data (void *, int);
-    void *read_data ();
-    int write_file (char *, char *);
-    int read_file (char *, char *);
+  int write_data(void *, int);
+  void *read_data();
+  int write_file(char *, char *);
+  int read_file(char *, char *);
 #endif
 
-    Node *accept (int);
-    int connect (const char *, int);
+  Node *accept(int);
+  int connect(const char *, int);
 
-    int process (int, job *, const char *);
-    void close(void);
-  };
+  int process(int, job *, const char *);
+  void close(void);
 };
+}; // namespace node
 #endif
